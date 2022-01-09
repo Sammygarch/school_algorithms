e4bdb6d340a9c801dd6b7ee17cf4889ca8919fd9
Sammygarch <s@mmygarcia.com>
2022-01-09 01:05:35 +0000

    1.2.3- minor improvements and documentation change
    changed documentation to numpy style - https://numpydoc.readthedocs.io/en/latest/format.html

--- src/school_algorithms/school_algorithms.py
+++ src/school_algorithms/school_algorithms.py
@@ -8,8 +8,8 @@ It aims to provide a simple way for secondary school students
 to learn and calculate equations that the often use.
 """
 
-from math import sqrt as _sqrt
-from math import pi as _pi
+from math import sqrt as _sqrt, pi as _pi
+
 
 def _if_not_int_or_float_raise(*args):
     for arg in args:
@@ -17,19 +17,31 @@ def _if_not_int_or_float_raise(*args):
             raise ValueError ("At least, one of the values wasn't an integer or a float.")
 
 def power_calc(E, t):
-    """
-    Calculates power from energy and time using the formula:
+    """Calculates power from energy and time using the formula:
         power = energy / time
 
-    Args:
-        E (int or float): The energy value in the equation
-        t (int or float): The time value of the equation
+    Parameters
+    ----------
+    E : int or float
+        The energy value in the equation.
 
-    Returns:
-        Float: E / t
+    t : int or float
+        The time value of the equation.
 
-    Raises:
-        ValueError: If E or t is not an integer or float
+    Returns
+    -------
+    Float
+         E / t
+
+    Raises
+    ------
+    ValueError
+        If E or t is not an integer or float
+
+    Examples
+    --------
+    >>> school_algorithms.power_calc(10, 5)
+    2.0
 
     """
     _if_not_int_or_float_raise(E, t)
@@ -40,15 +52,28 @@ def energy_calc(p, t):
     Calculates energy from power and time using the formula:
         energy = power * time
 
-    Args:
-        p (int or float): The power value of the equation
-        t (int or float): The time value of the equation
+    Parameters
+    ----------
+    p: Int or float
+        The power value of the equation.
 
-    Returns:
+    t: Int or float
+        The time value of the equation.
+
+    Returns
+    -------
+    Int
         p * t
 
-    Raises:
-        ValueError: If p or t is not an integer or float
+    Raises
+    ------
+    ValueError
+        If p or t is not an integer or float
+
+    Examples
+    --------
+    >>> school_algorithms.energy_calc(5, 2)
+    10
 
     """
     _if_not_int_or_float_raise(p, t)
@@ -59,15 +84,28 @@ def time_calc(p, E):
     Calculates time from power and energy using the formula:
         time = energy / power
 
-    Args:
-        p (int or float): The power value of the equation
-        E (int or float): The energy value of the equaton
+    Parameters
+    ----------
+    p: int or float
+        The power value of the equation.
 
-    Returns:
-        Float: E / p
+    E: int or float
+        The energy value of the equaton.
 
-    Raises:
-        ValueError: If p or E is not an integer or float
+    Returns
+    -------
+    Float
+        E / p
+
+    Raises
+    ------
+    ValueError
+        If p or E is not an integer or float
+
+    Examples
+    --------
+    >>> school_algorithms.energy_calc(10, 2)
+    5.0
 
     """
     _if_not_int_or_float_raise(p, E)
@@ -78,15 +116,27 @@ def pythag_hypot(a, b):
     Calculates the hypotenuse of a right angled triangle using the formula:
         hypotenuse = the square-root of (leg(a) squared + leg(b) squared)
 
-    Args:
-        a (int or float): leg(a) of the equation
-        b (int or float): leg(b) of the equation
+    Parameters
+    ----------
+    a: int or float
+        leg(a) of the equation.
+    b: int or float
+        leg(b) of the equation.
 
-    Returns:
-        Float: sqrt(a**2 + b**2)
+    Returns
+    -------
+    Float
+        sqrt(a**2 + b**2)
 
-    Raises:
-        ValueError: If a or b is not an integer or float
+    Raises
+    ------
+    ValueError
+        If a or b is not an integer or float
+
+    Examples
+    --------
+    >>> school_algorithms.pythag_hypot(4, 2)
+    4.47213595499958
 
     """
     _if_not_int_or_float_raise(a, b)
@@ -97,15 +147,28 @@ def pythag_leg(hy, a):
     Calculates the length of a leg in right-angled triangle using the formula:
         leg(b) = the square-root of (hy squared - leg(a) squared)
 
-    Args:
-        hy: the hypotneus of the equation
-        a: the leg of the equation
+    Parameters
+    ----------
+    hy: int or float
+        The hypotneus of the equation.
 
-    Returns:
-        Float: sqrt(a**2 + b**2)
+    a: int or float
+        The leg of the equation.
 
-    Raises:
-        ValueError: If hy or a is not an integer or float
+    Returns
+    -------
+    Float
+        sqrt(a**2 + b**2)
+
+    Raises
+    ------
+    ValueError
+        If hy or a is not an integer or float.
+
+    Examples
+    --------
+    >>> school_algorithms.pythag_leg(7, 2)
+    6.708203932499369
 
     """
     _if_not_int_or_float_raise(hy, a)
@@ -116,15 +179,29 @@ def triangle_area(b, h):
     Calculates the  area of a triangle using the formula:
         area = (base * height)/2
 
-    Args:
-        b(int or float): the base in the equation
-        h(int or float): the height in the equation
+    Parameters
+    ----------
+    b: int or float
+        The base in the equation.
 
-    Returns:
-        Float: (b*h) / 2
+    h: int or float
+        The height in the equation.
 
-    Raises:
-        ValueError: If b or h is not an integer or float
+    Returns
+    -------
+    Float
+        (b*h) / 2
+
+    Raises
+    ------
+    ValueError
+        If b or h is not an integer or float.
+
+    Examples
+    --------
+    >>> school_algorithms.triangle_area(4, 2)
+    4.0
+
     """
     _if_not_int_or_float_raise(b, h)
     return (b*h) / 2
@@ -134,16 +211,31 @@ def trapezium_area(c, b, h):
     Calculates the area of a trapezium using the formula:
         area = ((ceiling + base) / 2) * height
 
-    Args:
-        c(int or float): the ceiling in the equation
-        b(int or float): the base in the equation
-        h(int or float): the height in the equation
+    Parameters
+    ----------
+    c: int or float
+        The ceiling in the equation.
 
-    Returns:
-        Float: ((c + b) / 2) * h
+    b: int or float
+        The base in the equation.
 
-    Raises:
-        ValueError: If c b or h is not an integer or float
+    h: int or float
+        The height in the equation.
+
+    Returns
+    -------
+    Float
+        ((c + b) / 2) * h
+
+    Raises
+    ------
+    ValueError
+         If c b or h is not an integer or float
+
+    Examples
+    --------
+    >>> school_algorithms.trapezium_area(4, 5, 6)
+    27.0
 
     """
     _if_not_int_or_float_raise(c, b, h)
@@ -154,14 +246,26 @@ def circle_area(r):
     Calculates the area of a trapezium using the formula:
         area = \u03C0(radius squared)
 
-    Args:
-        r(int or float): the radius in the equation
+    Parameters
+    ----------
+    r: int or float
+        The radius in the equation.
 
-    Returns:
-        Float: \u03C0 * (r**2)
+    Returns
+    -------
+        Float
+            \u03C0 * (r**2)
 
-    Raises:
-        ValueError: If r is not an integer or float
+    Raises
+    ------
+    ValueError
+        If r is not an integer or float
+
+    Examples
+    --------
+    >>> school_algorithms.circle_area(10)
+    314.1592653589793
+
     """
     _if_not_int_or_float_raise(r)
     return _pi * (r**2)
